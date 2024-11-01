eb7afabfe77a809d6d7fe440963038e4b486fc5c
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2024-11-01 12:07:47 -0600

    Envío de Datos al Server Edge
    Co-Authored-By: Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>

--- lab09.py
+++ lab09.py
@@ -1,5 +1,7 @@
 import random
 import json
+from kafka import KafkaProducer
+import time
 
 def generarTemperatura():
   return round(random.uniform(0, 110), 2)
@@ -19,6 +21,30 @@ def generarMensaje():
   }
   return json.dumps(datos)
 
-for _ in range(5):
-  mensaje = generarMensaje()
-  print(mensaje)
+# Configuraciones para el servidor
+KAFKA_SERVER = 'lab9.alumchat.lol:9092'
+TOPIC = '21700'
+
+# Inicializar el Kafka Producer
+producer = KafkaProducer(
+  bootstrap_servers=KAFKA_SERVER,
+  value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+def enviarDatos():
+  try:
+    while True:
+      # Generar el mensaje
+      mensaje = json.loads(generarMensaje())
+      
+      # Enviar los datos al topic
+      producer.send(TOPIC, value=mensaje)
+      print(f"Datos enviados: {mensaje}")
+      
+      # Hacer un sleep de 15 a 30 segundos
+      tiempoEspera = random.uniform(15, 30)
+      time.sleep(tiempoEspera)
+  except KeyboardInterrupt:
+    print("Envío de datos interrumpido manualmente.")
+    
+enviarDatos()
